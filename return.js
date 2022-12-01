4810187f7981eeb4ba55106278fa4f6de558b7ae
fadhelarrizkyliranda <fadhelarrizkyliranda@gmail.com>
2022-12-01 15:06:21 +0700

    Penambahan Return

---
+++ return.js
@@ -0,0 +1,17 @@
+function bagi(a,b){
+    hasilBagi = a / b;
+    return hasilBagi;
+ }
+
+function tambah(a,b){
+    hasilTambah = a + b;
+    return hasilTambah;
+ }
+ 
+ var nilai1 = 20;
+ 
+ var nilai2 = 5;
+ 
+ var hasilPembagian = bagi(nilai1, nilai2);
+ var hasilPenambahan = tambah(hasilBagi,3);
+ console.log(hasilPenambahan);
